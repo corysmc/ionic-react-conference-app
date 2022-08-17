697600165a9c56db8de7a949c36d611ed91fd85c
Cory McArthur <cory.s.mcarthur@gmail.com>
2022-08-17 10:33:52 -0600

    adding demo of issue

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonMenuButton, IonRow, IonCol, IonButton, IonList, IonItem, IonLabel, IonText, IonTextarea, IonToast } from '@ionic/react';
-import './Login.scss';
+import React from 'react';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonMenuButton, IonButton  } from '@ionic/react';
 import { connect } from '../data/connect';
+import { Route, Switch } from 'react-router';
+import SupportModal from './SupportModal';
 
 interface OwnProps { }
 
@@ -10,26 +11,8 @@ interface DispatchProps { }
 interface SupportProps extends OwnProps, DispatchProps { }
 
 const Support: React.FC<SupportProps> = () => {
-
-  const [message, setMessage] = useState('');
-  const [formSubmitted, setFormSubmitted] = useState(false);
-  const [messageError, setMessageError] = useState(false);
-  const [showToast, setShowToast] = useState(false);
-
-  const send = (e: React.FormEvent) => {
-    e.preventDefault();
-    setFormSubmitted(true);
-    if (!message) {
-      setMessageError(true);
-    }
-    if (message) {
-      setMessage('');
-      setShowToast(true);
-    }
-  };
-
   return (
-    <IonPage id="support-page">
+    <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
@@ -40,40 +23,12 @@ const Support: React.FC<SupportProps> = () => {
       </IonHeader>
       <IonContent>
 
-        <div className="login-logo">
-          <img src="assets/img/appicon.svg" alt="Ionic logo" />
-        </div>
-
-        <form noValidate onSubmit={send}>
-          <IonList>
-            <IonItem>
-              <IonLabel position="stacked" color="primary">Enter your support message below</IonLabel>
-              <IonTextarea name="message" value={message} spellCheck={false} autocapitalize="off" rows={6} onIonChange={e => setMessage(e.detail.value!)}
-                required>
-              </IonTextarea>
-            </IonItem>
-
-            {formSubmitted && messageError && <IonText color="danger">
-              <p className="ion-padding-start">
-                Support message is required
-              </p>
-            </IonText>}
-          </IonList>
-
-          <IonRow>
-            <IonCol>
-              <IonButton type="submit" expand="block">Submit</IonButton>
-            </IonCol>
-          </IonRow>
-        </form>
+       <IonButton href="/support/new">New Support Ticket</IonButton>
        
       </IonContent>
-     
-      <IonToast
-        isOpen={showToast}
-        duration={3000}
-        message="Your support request has been sent"
-        onDidDismiss={() => setShowToast(false)} />
+     <Switch>
+        <Route path="/support/new" exact component={SupportModal} />
+     </Switch>
     </IonPage>
   );
 };
